d4c6c4b3dfae6896740e45df744d65cd6a136d0a
Ismail Kilicaslan <iskilicaslan61@gmail.com>
2021-12-12 17:22:56 +0100

    new  branch

--- src/my_git_repo/MyLocalClass1.java
+++ src/my_git_repo/MyLocalClass1.java
@@ -1,18 +1,10 @@
 package my_git_repo;
 
-public class FirstJava {
+public class MyLocalClass1 {
 
 	public static void main(String[] args) {
-
-		int x=2;
-
-		System.out.println(x);
-
 		
 		
-		
-		
-
 
 	}
 
